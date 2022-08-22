dae8302b8805ddb65d51ec251e2844a0d2bc0982
Zohaib <zohaibmanzoor1700@gmail.com>
2022-08-22 10:00:58 -0700

    fluency 01

---
+++ Fluency-01.js
@@ -0,0 +1,19 @@
+// Write a function, isCapitalized, to test the first character of a string is uppercase or not.
+const isCapitalized = (string) => {
+
+}
+
+const isCapitalizedtest1 = 'Hello' // True
+const isCapitalizedtest2 = 'hello' // False
+const isCapitalizedtest3 = 'hELLO' // False
+console.log(isCapitalized(isCapitalizedTest1))
+
+// Write a JavaScript function, countVowels, to count the number of vowels in a given string
+const countVowels = (string) => {
+
+}
+
+const countVowelsTest1 = 'Count all the vowels in this string' 
+const countVowelsTest2 = 'Even with characters like : and ?'
+const countVowelsTest3 = 'aeiouL' // 5
+console.log(countVowels(countVowelsTest1))
