c29f8ee4a908797c1a950996da136fdb2af693b5
Brian Allen <vecter1180@gmail.com>
2022-08-22 14:09:08 -0400

    completed half

--- Fluency-01.js
+++ Fluency-01.js
@@ -1,5 +1,10 @@
 // Write a function, isCapitalized, to test the first character of a string is uppercase or not.
 const isCapitalized = (string) => {
+    if(string = string.slice(1) + string.toUpperCase ){
+        return true
+    }else{
+        return false
+    }
 
 }
 
@@ -10,7 +15,8 @@ console.log(isCapitalized(isCapitalizedTest1))
 
 // Write a JavaScript function, countVowels, to count the number of vowels in a given string
 const countVowels = (string) => {
-
+    let countVowels = string.match
+    Text.match(/aeiou/)
 }
 
 const countVowelsTest1 = 'Count all the vowels in this string' 
