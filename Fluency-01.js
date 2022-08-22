98f2fcea42e2296f4264311fae3738d1fb09f1b2
Brian Allen <vecter1180@gmail.com>
2022-08-22 14:47:51 -0400

    completed 1 question

--- Fluency-01.js
+++ Fluency-01.js
@@ -1,25 +1,25 @@
 // Write a function, isCapitalized, to test the first character of a string is uppercase or not.
 const isCapitalized = (string) => {
-    if(string = string.slice(1) + string.toUpperCase ){
-        return true
-    }else{
-        return false
+    if (string[0] === string[0].toUpperCase()) {
+      return true;
+    } else {
+      return false;
     }
-
-}
-//this is a trail
-const isCapitalizedtest1 = 'Hello' // True
-const isCapitalizedtest2 = 'hello' // False
-const isCapitalizedtest3 = 'hELLO' // False
-console.log(isCapitalized(isCapitalizedTest1))
-
-// Write a JavaScript function, countVowels, to count the number of vowels in a given string
-const countVowels = (string) => {
-    let countVowels = string.match
-    Text.match(/aeiou/)
-}
-
-const countVowelsTest1 = 'Count all the vowels in this string' 
-const countVowelsTest2 = 'Even with characters like : and ?'
-const countVowelsTest3 = 'aeiouL' // 5
-console.log(countVowels(countVowelsTest1))
+  };
+  
+  const isCapitalizedtest1 = "Hello"; // True
+  const isCapitalizedtest2 = "hello"; // False
+  const isCapitalizedtest3 = "hELLO"; // False
+  console.log(isCapitalized(isCapitalizedtest2));
+  
+  // Write a JavaScript function, countVowels, to count the number of vowels in a given string
+  const countVowels = (string) => {
+    let countVowels = string.match;
+    //  Text.match(/aeiou/)
+  };
+  
+  const countVowelsTest1 = "Count all the vowels in this string";
+  const countVowelsTest2 = "Even with characters like : and ?";
+  const countVowelsTest3 = "aeiouL"; // 5
+  console.log(countVowels(countVowelsTest1));
+  
